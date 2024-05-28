6247f44bd7d3003d1ae2e7a46a115fa5e3fa7cc7
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2024-05-28 10:21:32 +0200

    Add files via upload

--- Codee/app/Classes/Artikel.php
+++ Codee/app/Classes/Artikel.php
@@ -1,24 +1,45 @@
 <?php
+// acteur Nouaman Guendoul
+namespace Bas\Classes;
+
 class Artikel {
     private $pdo;
 
-    public function __construct($pdo) {
+    public function __construct(\PDO $pdo) {
         $this->pdo = $pdo;
     }
 
-    public function createArtikel($artOmschrijving, $artInkoop, $artVerkoop, $artVoorraad, $artMinVoorraad, $artMaxVoorraad, $artLocatie) {
-        $sql = "INSERT INTO ARTIKEL(artOmschrijving, artInkoop, artVerkoop, artVoorraad, artMinVoorraad, artMaxVoorraad, artLocatie) 
-                VALUES (:artOmschrijving, :artInkoop, :artVerkoop, :artVoorraad, :artMinVoorraad, :artMaxVoorraad, :artLocatie)";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([
-            ':artOmschrijving' => $artOmschrijving,
-            ':artInkoop' => $artInkoop,
-            ':artVerkoop' => $artVerkoop,
-            ':artVoorraad' => $artVoorraad,
-            ':artMinVoorraad' => $artMinVoorraad,
-            ':artMaxVoorraad' => $artMaxVoorraad,
-            ':artLocatie' => $artLocatie
-        ]);
+    public function createArtikel($omschrijving, $inkoop, $verkoop, $voorraad, $minVoorraad, $maxVoorraad, $locatie) {
+        try {
+            $stmt = $this->pdo->prepare("
+                INSERT INTO artikel 
+                (artOmschrijving, artInkoop, artVerkoop, artVoorraad, artMinVoorraad, artMaxVoorraad, artLocatie) 
+                VALUES 
+                (?, ?, ?, ?, ?, ?, ?)
+            ");
+            $stmt->execute([
+                $omschrijving, $inkoop, $verkoop, $voorraad, 
+                $minVoorraad, $maxVoorraad, $locatie
+            ]);
+            
+            // Controleer of de invoeging succesvol was
+            if ($stmt->rowCount() == 0) {
+                throw new \Exception('Het invoegen van het artikel is mislukt.');
+            }
+        } catch (\Exception $e) {
+            echo 'Fout bij het invoegen van artikel: ',  $e->getMessage(), "\n";
+        }
+    }
+
+    
+    public function getAllArtikelen() {
+        try {
+            $stmt = $this->pdo->query("SELECT artOmschrijving, artInkoop, artVerkoop, artVoorraad, artMinVoorraad, artMaxVoorraad, artLocatie FROM artikel");
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\Exception $e) {
+            echo 'Fout bij het ophalen van artikelen: ',  $e->getMessage(), "\n";
+            return [];
+        }
     }
 }
-?>
+
